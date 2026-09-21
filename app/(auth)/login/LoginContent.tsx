f6d2597b3b324d438165a39b25bb8227cbed7dff
jack <jitendrakumarbindra760@gmail.com>
2026-09-21 23:49:25 +0530

    fixed build issue

--- app/(auth)/login/LoginContent.tsx
+++ app/(auth)/login/LoginContent.tsx
@@ -2,7 +2,7 @@ import { Suspense } from "react";
 import LoginContent from "./LoginContent";
 
 export default function LoginPage() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}> <LoginContent /> </Suspense>
-  );
+    return (
+        <Suspense fallback={<div>Loading...</div>}> <LoginContent /> </Suspense>
+    );
 }
